5442747eea0ef64e318cc521badebdb5f9043f25
Maxwell Croy <max.croy1@gmail.com>
2020-05-22 17:21:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -2,6 +2,7 @@ def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   my_hash = {}
   my_hash[key] = value
+  my_hash
 end
 
 def read_from_hash(hash, key)
